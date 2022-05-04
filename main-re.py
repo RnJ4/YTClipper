c51a1a7c99a096bda921870b0b902d09be75f7fa
RnJ4 <30072955+RnJ4@users.noreply.github.com>
2022-05-04 11:52:28 +0800

    Create main-re.py

---
+++ main-re.py
@@ -0,0 +1,24 @@
+from ast import arg
+import os,argparse,subprocess
+import string
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--url", help="video url",type=str)
+parser.add_argument("--start", help="start time",type=str)
+parser.add_argument("--end", help="end time",type=str)
+args = parser.parse_args()
+cmd="yt-dlp -g -S \"ext\" "+args.url
+
+time=" -ss "+args.start+" -to "+args.end+" "
+
+r = os.popen(cmd)  
+urlOutput = r.read()  
+r.close()
+
+urls=urlOutput.split('\n',1)
+# print("first: "+urls[0])
+# print("second: "+urls[1])
+trimCmd="./ffmpeg"+time+"-i \""+urls[0]+"\""+time+"-i "+"\""+urls[1]+"\"  output.mp4"
+print(trimCmd)
+subprocess.run(trimCmd,shell=True)
