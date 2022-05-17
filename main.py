ebea50e20f723034344491674ec10a2c15af2c3a
RnJ4 <30072955+RnJ4@users.noreply.github.com>
2022-05-18 01:01:04 +0800

    Skip dash manifest
    Skip dash manifest because ffmpeg seems not working with it.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ parser.add_argument("--url", help="video url",type=str)
 parser.add_argument("--start", help="start time",type=str)
 parser.add_argument("--end", help="end time",type=str)
 args = parser.parse_args()
-cmd="yt-dlp -g -S \"ext\" "+args.url
+cmd="yt-dlp -g --youtube-skip-dash-manifest -S \"ext\" "+args.url
 
 time=" -ss "+args.start+" -to "+args.end+" "
 
@@ -17,8 +17,12 @@ urlOutput = r.read()
 r.close()
 
 urls=urlOutput.split('\n',1)
+print(len(urls))
 # print("first: "+urls[0])
 # print("second: "+urls[1])
-trimCmd="./ffmpeg"+time+"-i \""+urls[0]+"\""+time+"-i "+"\""+urls[1]+"\" -c copy output.mp4"
+trimCmd="./ffmpeg"+time+"-i \""+urls[0]+"\""
+if(len(urls[1])>0):
+    trimCmd+=time+"-i "+"\""+urls[1]+"\""
+trimCmd+=" -c copy output.mp4"
 print(trimCmd)
 subprocess.run(trimCmd,shell=True)
