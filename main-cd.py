5d92ffd2c432e40c00256a4b395e010531c12cfa
RnJ4 <30072955+RnJ4@users.noreply.github.com>
2022-05-18 14:44:21 +0800

    Update main-cd.py

--- main-cd.py
+++ main-cd.py
@@ -14,14 +14,7 @@ time=" -ss "+args.start+" -to "+args.end+" "
 
 subprocess.run(cmd,shell=True)
 
-# urls=urlOutput.split('\n',1)
-# print(len(urls))
-# print("first: "+urls[0])
-# print("second: "+urls[1])
-# trimCmd="./ffmpeg"+time+"-i \""+urls[0]+"\""
-# if(len(urls[1])>0):
-#     trimCmd+=time+"-i "+"\""+urls[1]+"\""
-# trimCmd+=" -c copy output.mp4"
+
 trimCmd="./ffmpeg " + time +"-i temp.mp4 -c copy output.mp4"
 print(trimCmd)
 subprocess.run(trimCmd,shell=True)
